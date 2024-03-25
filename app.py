0968e1b35f9ec455a6cfd4f384d17d2e5094ef46
jacoblvf <163808026+jacoblvf@users.noreply.github.com>
2024-03-25 17:42:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ if refresh_button:
     
 @st.cache_data
 def convert_df(df7):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df7)
+csv = convert_df(my_large_df)
 
 st.download_button(
     label="Download data as CSV",
     data=csv,
-    file_name='MAinventory.csv',
+    file_name='large_df.csv',
     mime='text/csv',
 )
